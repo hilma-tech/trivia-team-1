74721d20e293b6a5c954ac16a9f8285707ca951e
boazfr1 <boazfr1@gmail.com>
2023-02-22 14:07:38 +0200

    files from main

---
+++ client/src/components/Background.tsx
@@ -0,0 +1,26 @@
+import { ContainerProps } from "@mui/material";
+import rightLeaf from "../images/backgroundLeaves/rightLeaf.svg";
+import leftLeaf from "../images/backgroundLeaves/leftLeaf.svg";
+import bottomLeaf from "../images/backgroundLeaves/bottomLeaf.svg"
+
+
+const Background = (props: ContainerProps) => (
+  window.innerWidth > 450 ? (
+    <div className="backgroundContainer">
+      <img id="rightLeaf" src={rightLeaf} />
+      <img id="leftLeaf" src={leftLeaf} />
+      <div id="compChildrenContainer">
+        {props.children}
+      </div>
+    </div>
+  ) : (
+    <div className = "backgroundContainerPhone"> 
+      <div>
+        {props.children}
+      </div>
+      <img id="bottomLeaf" src={bottomLeaf} />
+    </div>
+  )
+);
+
+export default Background;
