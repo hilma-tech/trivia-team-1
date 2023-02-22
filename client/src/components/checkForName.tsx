c7c2a4ba676a8d099941a886cb34a51dcb1686cc
boazfr1 <boazfr1@gmail.com>
2023-02-22 11:37:31 +0200

    before taking designing

---
+++ client/src/components/checkForName.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import triangleIcon from '../icons/Icon-awesome-play.png';
+
+
+interface CheckForNameProps {
+    quizTitle: string
+}
+
+const CheckForName: React.FC<CheckForNameProps> = ({ quizTitle }) => {
+
+    const [playerName, setPlayerName] = useState("");
+
+    function sendNameOfPlayerToServer() {
+        fetch(`#`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ playerName: playerName })
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                moveToPlayGame();
+            })
+            .catch((err) => {
+                console.log("sendNameOfPlayerToServer");
+                console.log(err, "catch");
+            })
+    }
+
+    const moveToPlayGame = () => {
+        // navigate();
+        console.log("navigate");
+    }
+
+    return (
+        <main>
+            <div className='main-CheckForName-for-flex'>
+                <div className='main-CheckForName'>
+                    <div className='title-and-paragraph'>
+                        <h1 className='quizTitle'>{quizTitle}</h1>
+                        <p>איך קוראים לכם?</p>
+                    </div>
+                    <form name="identification-form">
+                        <input onChange={(ev) => setPlayerName(ev.target.value)} name="player-identification" type="text"
+                            placeholder="נא להזין שם" value={playerName} required />
+                    </form>
+                    <button className='CheckForName-button' onClick={sendNameOfPlayerToServer}>
+                        <p>!יאללה בואו נתחיל</p>
+                        <img src={`${triangleIcon}`} alt="icon of triangle" />
+                    </button>
+                </div>
+            </div>
+        </main>
+    );
+}
+export default CheckForName;
---
+++ client/src/components/opening-for-the-quiz.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import '../style/OpeningForTheQuiz.css';
+import triangleIcon from '../icons/Icon-awesome-play.png';
+import italyPic from '../img/italy.png'
+import OpeningParagraphTitle from './opening-paragraph-title';
+import CheckForName from './checkForName';
+
+function OpeningForTheQuiz() {
+
+    const [imgUrl, setImgUrl] = useState("https://www.skideal.co.il/app/uploads/2020/06/Rome-e1591105892246.jpg");
+    const [quizTitle, setQuizTitle] = useState("איטליהה מה אתם יודעים?");
+    const [paragraph, setParagraph] = useState("לפני הטיסה לאיטליה רציתי לעשות לכם חידון על הארץ המיוחדת הזאת.. מהצפון ועד לדרום מה אתם יודעים? אוהבתת");
+    const [changeComponent, setChangeComponent] = useState(false);
+
+    useEffect(() => {
+        getInfoFromServer();
+    }, []);
+
+    const getInfoFromServer = async () => {
+        await fetch(`#`)
+            // get image, title, and paragraph for server and push them into state
+            .then((res) => res.json)
+            .then((data) => {
+                console.log("hiiiii");
+                // setImgUrl(data.imgUrl);
+                // setQuizTitle(data.quizTitle);
+                // setParagraph(data.paragraph);
+            })
+            .catch((err) => {
+                console.log(err, "catch");
+            })
+    }
+
+    return (
+        <main>
+            {changeComponent ?
+                <OpeningParagraphTitle
+                    quizTitle={quizTitle}
+                    paragraph={paragraph}
+                    imgUrl={imgUrl}
+                    changeComponent = {changeComponent}
+                    setChangeComponent={setChangeComponent}
+                />
+                :
+                <CheckForName quizTitle={quizTitle} />
+            }
+        </main>
+    );
+}
+
+export default OpeningForTheQuiz;
