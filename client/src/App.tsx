00e29fb56fb76a55569714dcf449f2f1f13c5dca
hilma <ohadoff2@gmail.com>
2023-02-26 10:58:47 +0200

    temporary initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import EditQuiz from './components/EditQuiz';
 import AnswersProvider from './context/AnswersContext';
 import './App.css';
+import EditQuizMobile from './components/edit-quiz-mobile/EditQuizMobile';
 
 function App() {
   return (
     <div className="App">
       <AnswersProvider>
-        <EditQuiz />
+        <EditQuizMobile />
       </AnswersProvider>
     </div>
   );
--- client/src/components/edit-quiz-mobile/EditQuizMobile.tsx
+++ client/src/components/edit-quiz-mobile/EditQuizMobile.tsx
@@ -1,28 +1,27 @@
 import React, { Component, FC, useState, createContext, useEffect } from 'react';
-import '../style/EditQuiz.scss'
-import LeftLeaf from '../images/leftleaf.svg'
-import RightLeaf from '../images/rightleaf.svg'
-import ShowQuizBtn from '../images/showquizzbtn.svg'
-import LinkBtn from '../images/linkBtn.svg'
-import saveBtn from '../images/saveBtn.svg'
-import Selectimage from '../images/image.svg'
-import AddQuestionBox from './AddQuestionBox'
-import plusBtn from '../images/plusBtn.svg'
-import FinalBoxQuestions from './FinalBoxQuestions'
-import { useAnswerContext } from '../context/AnswersContext'
-import { CurrentQuestion } from '../utils/Interfaces'
+// import '../css/EditQuiz.scss'
 
-import AnswersProvider from '../context/AnswersContext'
+import ShowQuizBtn from '../../images/showquizzbtn.svg'
+import LinkBtn from '../../images/linkBtn.svg'
+import saveBtn from '../../images/saveBtn.svg'
+import Selectimage from '../../images/image.svg'
+import AddQuestionBox from '../AddQuestionBox';
+import plusBtn from '../../images/plusBtn.svg'
+import FinalBoxQuestions from '../FinalBoxQuestions';
+import { useAnswerContext } from '../../context/AnswersContext'
+import { CurrentQuestion } from '../../utils/Interfaces'
 
+import '../../style/EditQuiz.scss'
 
 
 
 
 
-const EditQuiz: FC = () => {
 
-    const {  setQuestions, questions, setEmptyQuestionEdit, emptyQuestionEdit } = useAnswerContext()
+const EditQuizMobile: FC = () => {
+    const { sendInput, setQuestions, questions, setSendInput, setEmptyQuestionEdit, emptyQuestionEdit } = useAnswerContext()
 
+    // const [questionsArr, setQuestionsArr] = useState([1]);
     const [currentEditQuestion, setCurrentEditQuestion] = useState(0);
     const [currentQuestion, setCurrentQuestion] = useState<CurrentQuestion>({
         questionId: 1,
@@ -30,15 +29,23 @@ const EditQuiz: FC = () => {
         answers: ['', '']
     });
 
+    const [countForKeys, setCountForKeys] = useState(0);
+
+
 
     useEffect(() => {
         setQuestions([...questions, currentQuestion])
-    }, [])
+    }, [sendInput])
 
 
     const addQuestion = () => {
-        setEmptyQuestionEdit(!emptyQuestionEdit)
+        // setEmptyQuestionEdit(!emptyQuestionEdit)
+        setSendInput(!sendInput)
         if (questions.length < 10) {
+            // setCurrentQuestion(prevState => ({
+            //     ...prevState,
+            //     questionId: prevState.questionId + 1
+            //   }));
             setQuestions([...questions, currentQuestion])
             setCurrentEditQuestion(currentEditQuestion + 1)
             setCurrentQuestion(prevState => ({
@@ -57,11 +64,7 @@ const EditQuiz: FC = () => {
 
     return (
         <>
-            <div className='leftleafContainer'>
-                <img className='leftleaf' src={LeftLeaf} alt="left leaf" />
-                <img className='rightleaf' src={RightLeaf} alt="right leaf" />
-            </div>
-            <div className='formContanier'>
+            <div className='formContanier active'>
                 <div className='topContainer'>
                     <div className='topButtonsContainer'>
                         <div className='topRightbtn'>
@@ -79,7 +82,8 @@ const EditQuiz: FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className='quizHeaderContainer'>
+
+                <div className='quizHeaderContainer hide' >
                     <div className='quizHeaderImage'> <img className='selectImageQuizSvg' src={Selectimage} /></div>
                     <div className='titleHeaderContainer'>
                         <h1>חידון ללא כותרת</h1>
@@ -93,6 +97,7 @@ const EditQuiz: FC = () => {
                             : <FinalBoxQuestions key={index} questionId={index + 1} />
                     )
                 })}
+                {/* <AddQutionsBox /> */}
                 <div className='plus-btn-container'>
                     <button className='plus-btn' onClick={addQuestion}>
                         <img src={plusBtn} className='plus-btn-svg' />
@@ -103,4 +108,4 @@ const EditQuiz: FC = () => {
     );
 }
 
-export default EditQuiz;
+export default EditQuizMobile;
