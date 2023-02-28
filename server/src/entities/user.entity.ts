9a185ae24487626b747b7e75f400b12c5765e79b
KorenO17 <ofkorofir@gmail.com>
2023-02-28 17:33:16 +0200

    create register api route

--- server/src/entities/user.entity.ts
+++ server/src/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { Quiz } from "./quiz.entity";
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany, OneToOne } from "typeorm";
 import { Password } from "./password.entity";
-import { Score } from "./score.entity";
+
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
@@ -10,10 +10,9 @@ export class User {
     @Column()
     username: string;
 
-    @OneToMany(() => Quiz, (quizzes) => quizzes.creator, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @OneToMany(() => Quiz, (quizzes) => quizzes.creator, { onDelete: 'CASCADE', onUpdate: 'CASCADE', cascade: true })
     quizzes: Quiz[]
 
-    @OneToOne(() => Password, (password) => password.user, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @OneToOne(() => Password, (password) => password.user, { onDelete: 'CASCADE', onUpdate: 'CASCADE', cascade: true })
     password: Password
-
 }
