8a9afdc0d5e6053a4e96310b3a1bae228564f6e0
ej-shafran <ejshafran@hilma.tech>
2023-03-09 12:17:07 +0200

    Remove extra dumps

--- dumps/dump_temp/main.sh
+++
@@ -1,6 +0,0 @@
-mysql -uroot -pz10mz10m monkey_quiz < ./monkey_quiz_answer.sql &&
-mysql -uroot -pz10mz10m monkey_quiz < ./monkey_quiz_question.sql &&
-mysql -uroot -pz10mz10m monkey_quiz < ./monkey_quiz_quiz.sql &&
-mysql -uroot -pz10mz10m monkey_quiz < ./monkey_quiz_score.sql &&
-mysql -uroot -pz10mz10m monkey_quiz < ./monkey_quiz_user.sql;
-
