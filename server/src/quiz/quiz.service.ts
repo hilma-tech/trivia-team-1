d61ff51ba80b85d76ba60af309e144044d72047a
hilma <amitgonen2468@gmail.com>
2023-03-13 11:36:46 +0200

    add

--- server/src/quiz/quiz.service.ts
+++ server/src/quiz/quiz.service.ts
@@ -13,37 +13,26 @@ export class QuizService {
   constructor(
     @InjectRepository(Quiz)
     private readonly quizRepository: Repository<Quiz>,
-  ) {}
+  ) { }
 
   async editQuiz(id: number, quiz: QuizDTO) {
     await this.quizRepository.save({ id: id, ...quiz });
   }
 
-    async addQuiz(quiz: QuizDTO) {
-        const { questions, creatorId, ...rest } = quiz;
+  async addQuiz(quiz: QuizDTO) {
+    const { questions, creatorId, ...rest } = quiz;
 
-        const newQuestions = questions.map((question) => {
-            return {
-                title: question.title,
-                answers: question.answers,
-                imageUrl: question.imageUrl
-            }
-        })
+    const newQuestions = questions.map((question) => {
+      return {
+        title: question.title,
+        answers: question.answers,
+        imageUrl: question.imageUrl
+      }
+    })
 
-        return this.quizRepository.save({ questions: newQuestions, creator: { id: creatorId }, ...rest })
-    }
-    // async editQuiz(id: number, quiz: QuizDTO) {
+    return this.quizRepository.save({ questions: newQuestions, creator: { id: creatorId }, ...rest })
+  }
 
-    //     const newQuestions = questions.map((question) => {
-    //         return {
-    //             title: question.title,
-    //             answers: question.answers,
-    //             imageUrl: question.imageUrl
-    //         }
-    //     })
-
-    //     return this.quizRepository.save({ questions: newQuestions, creator: { id: creatorId }, ...rest })
-    // }
 
   async highScores(quizId: number) {
     const res = await this.quizRepository.findOne({
@@ -67,86 +56,86 @@ export class QuizService {
     });
     return quiz;
   }
-   
 
-    //TODO: temporary
-    // this should be a string[] when using @hilma/auth
-    addFakeData(userIds: number[], amount: number) {
-        const quizzes = userIds.flatMap((id) => {
-            const userQuizzes: DeepPartial<Quiz>[] = [];
-            for (let i = 0; i < amount; i++) {
-                userQuizzes.push(this.randomQuiz(id));
-            }
-            return userQuizzes;
-        });
-        this.quizRepository.save(quizzes);
+
+  //TODO: temporary
+  // this should be a string[] when using @hilma/auth
+  addFakeData(userIds: number[], amount: number) {
+    const quizzes = userIds.flatMap((id) => {
+      const userQuizzes: DeepPartial<Quiz>[] = [];
+      for (let i = 0; i < amount; i++) {
+        userQuizzes.push(this.randomQuiz(id));
+      }
+      return userQuizzes;
+    });
+    this.quizRepository.save(quizzes);
+  }
+
+  //TODO: temporary
+  // this should be a string when using @hilma/auth
+  randomQuiz(id: number) {
+    const quiz: DeepPartial<Quiz> = {
+      creator: { id },
+      description: faker.commerce.productDescription(),
+      title: faker.commerce.productName(),
+      imageUrl: faker.image.imageUrl(640, 480, undefined, true),
+      questions: [],
+      scores: []
     }
 
-    //TODO: temporary
-    // this should be a string when using @hilma/auth
-    randomQuiz(id: number) {
-        const quiz: DeepPartial<Quiz> = {
-            creator: { id },
-            description: faker.commerce.productDescription(),
-            title: faker.commerce.productName(),
-            imageUrl: faker.image.imageUrl(640, 480, undefined, true),
-            questions: [],
-            scores: []
-        }
-
-        const amountOfQuestions = faker.datatype.number({min: 3, max: 10});
-        for (let i = 0; i < amountOfQuestions; i++) {
-            quiz.questions.push(this.randomQuestion() as Question);
-        }
-
-        const amountOfScores = faker.datatype.number({min: 3, max: 15});
-        for (let i = 0; i < amountOfScores; i++) {
-            quiz.scores.push(this.randomScore() as Score);
-        }
-
-        return quiz;
+    const amountOfQuestions = faker.datatype.number({ min: 3, max: 10 });
+    for (let i = 0; i < amountOfQuestions; i++) {
+      quiz.questions.push(this.randomQuestion() as Question);
     }
 
-    //TODO: temporary
-    randomQuestion() {
-        const question: DeepPartial<Question> = {
-            imageUrl: faker.image.imageUrl(640, 480, undefined, true),
-            title: faker.commerce.productName(),
-            answers: []
-        }
-
-        const amountOfAnswers = faker.datatype.number({ min: 2, max: 4})
-        const correctIndex = faker.datatype.number({min: 0, max: amountOfAnswers});
-        for (let i = 0; i < amountOfAnswers; i++) {
-            question.answers.push(this.randomAnswer(correctIndex === i) as Answer);
-        }
-
-        return question;
+    const amountOfScores = faker.datatype.number({ min: 3, max: 15 });
+    for (let i = 0; i < amountOfScores; i++) {
+      quiz.scores.push(this.randomScore() as Score);
     }
 
+    return quiz;
+  }
 
-    //TODO: temporary
-    randomAnswer(isCorrect: boolean) {
-        const answer: DeepPartial<Answer> = {
-            imageUrl: faker.image.imageUrl(640, 480, undefined, true),
-            isCorrect,
-            text: faker.commerce.productName(),
-        }
-
-        return answer;
+  //TODO: temporary
+  randomQuestion() {
+    const question: DeepPartial<Question> = {
+      imageUrl: faker.image.imageUrl(640, 480, undefined, true),
+      title: faker.commerce.productName(),
+      answers: []
     }
 
-    //TODO: temporary
-    randomScore() {
-        const score: DeepPartial<Score> = {
-            player: faker.name.fullName(),
-            score: faker.datatype.number({min: 0, max: 100})
-        }
-
-        return score;
+    const amountOfAnswers = faker.datatype.number({ min: 2, max: 4 })
+    const correctIndex = faker.datatype.number({ min: 0, max: amountOfAnswers });
+    for (let i = 0; i < amountOfAnswers; i++) {
+      question.answers.push(this.randomAnswer(correctIndex === i) as Answer);
     }
 
-    async deleteQuiz(quizId: number) {
-        await this.quizRepository.delete( quizId );
-        }
+    return question;
+  }
+
+
+  //TODO: temporary
+  randomAnswer(isCorrect: boolean) {
+    const answer: DeepPartial<Answer> = {
+      imageUrl: faker.image.imageUrl(640, 480, undefined, true),
+      isCorrect,
+      text: faker.commerce.productName(),
+    }
+
+    return answer;
+  }
+
+  //TODO: temporary
+  randomScore() {
+    const score: DeepPartial<Score> = {
+      player: faker.name.fullName(),
+      score: faker.datatype.number({ min: 0, max: 100 })
+    }
+
+    return score;
+  }
+
+  async deleteQuiz(quizId: number) {
+    await this.quizRepository.delete(quizId);
+  }
 }
